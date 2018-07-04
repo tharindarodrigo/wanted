3fbab21fb54c1564dc569c8eed912bb8fa719316
tharidnarodrigo <tharindarodrigo@gmail.com>
2018-07-05 01:24:53 +0530

    Initial commit

---
+++ scripts/demo.js
@@ -0,0 +1,19 @@
+
+// Initialize Share-Buttons
+$.shareButtons({
+  effect  : 'slide-on-scroll',
+  buttons : {
+
+
+    'facebook':   { class: 'facebook',  use: true, icon: 'facebook',    link: 'https://www.facebook.com/Wantedlk-1205892636214312/', extras: 'target="_blank"' },
+    'google':     { class: 'gplus',     use: true, icon: 'google-plus', link: 'https://plus.google.com/u/0/102301263864764439633', extras: 'target="_blank"' },
+    'linkedin':   { class: 'linkedin',  use: true, icon: 'linkedin',    link: 'https://www.linkedin.com/in/arrow-engineering-wanted-993422167/', extras: 'target="_blank"' },
+    'twitter':    { class: 'twitter',   use: true, icon: 'twitter',     link: 'https://twitter.com/lk_wanted', extras: 'target="_blank"' },
+    'pinterest':  { class: 'pinterest', use: true, icon: 'pinterest',   link: 'https://www.pinterest.com/wantedlk/', extras: 'target="_blank"' },
+    'instagram':  { class: 'instagram', use: true, icon: 'instagram',   link: 'https://www.instagram.com/arrowengineering.wanted.lk/', extras: 'target="_blank"' },
+        
+   
+    'phone':      { class: 'phone',     use: true, icon: 'phone',       link: '',  type: 'phone' },
+        
+  }
+});
